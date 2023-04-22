832c362092d19179d048a3b34b7a506abef2427f
allen kim <allenkim@allens-MacBook-Air.local>
2023-04-22 14:18:03 -0400

    changed index to template

--- page-route/build-pages-data.js
+++ page-route/build-pages-data.js
@@ -6,22 +6,18 @@ import { encode, walkDir, setNestedVal } from './util.js';
 
 const [_0, _1, pagesPath='./pages', outputPath='./public/pages.data'] = process.argv;
 
-function main(pagesPath, outputPath) {
-  const pagesVar = {};
+const pagesVar = {};
 
-  walkDir(pagesPath, filePath => {
-    const url = path.resolve(filePath).replace(new RegExp(`^${path.resolve(pagesPath)}`), '');
-    if (fs.statSync(filePath).isFile()) {
-      const keys = url.replace(/\//g, '.').replace(/^\.|\.html$/g, '');
-      const fileContents = fs.readFileSync(filePath, 'utf8');
-      setNestedVal(pagesVar, keys, fileContents);
-    }
-  });
+walkDir(pagesPath, filePath => {
+  const url = path.resolve(filePath).replace(new RegExp(`^${path.resolve(pagesPath)}`), '');
+  if (fs.statSync(filePath).isFile()) {
+    const keys = url.replace(/\//g, '.').replace(/^\.|\.html$/g, '');
+    const fileContents = fs.readFileSync(filePath, 'utf8');
+    const value = keys.endsWith('/template') ? fileContents + '<slot></slot>' : fileContents;
+    setNestedVal(pagesVar, keys, value);
+  }
+});
 
-  const json = JSON.stringify(pagesVar);
-  const encoded = encode(json);
-  fs.writeFileSync(path.join(outputPath), encoded);
-  // console.log(JSON.stringify(pagesVar, null, '  '));
-}
-
-main(pagesPath, outputPath);
+const json = JSON.stringify(pagesVar);
+const encoded = encode(json);
+fs.writeFileSync(path.join(outputPath), encoded);
